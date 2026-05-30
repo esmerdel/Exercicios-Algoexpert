279a371ed9cebc8d594f3a6cfd035ed39b433a30
esmerdel <leandro_esmerdel@hotmail.com>
2026-05-29 23:58:30 -0300

    Longest Peak

---
+++ arrays/longest-peak.py
@@ -0,0 +1,18 @@
+def longestPeak(array):
+
+    maiorPico = 0
+    for i in range(1, len(array) - 1 ):
+
+        if array[i] > array[i - 1] and array[i] > array[i + 1]:
+            left = i - 1
+            right = i + 1
+            while left > 0 and array[left] > array[left - 1]:
+                left -= 1
+            
+            while right < len(array) - 1 and array[right] > array[right + 1]:
+                right += 1
+
+            pico = right - left + 1
+            if maiorPico < pico:
+                maiorPico = pico
+    return maiorPico
